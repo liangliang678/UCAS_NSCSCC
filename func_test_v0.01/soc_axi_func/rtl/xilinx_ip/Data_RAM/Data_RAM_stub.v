62675193ba596f6d6cb94e8f33271c339878cbc4
吴俊亮 <wujunliang18@mails.ucas.ac.cn>
2021-06-06 11:01:41 +0800

    add ip for cache in func test

---
+++ func_test_v0.01/soc_axi_func/rtl/xilinx_ip/Data_RAM/Data_RAM_stub.v
@@ -0,0 +1,25 @@
+// Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2019.2 (win64) Build 2708876 Wed Nov  6 21:40:23 MST 2019
+// Date        : Sat Jun  5 23:06:47 2021
+// Host        : DESKTOP running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub
+//               e:/NSCSCC/func_test_v0.01/soc_axi_func/rtl/xilinx_ip/Data_RAM/Data_RAM_stub.v
+// Design      : Data_RAM
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7a200tfbg676-2
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* x_core_info = "blk_mem_gen_v8_4_4,Vivado 2019.2" *)
+module Data_RAM(clka, ena, wea, addra, dina, douta)
+/* synthesis syn_black_box black_box_pad_pin="clka,ena,wea[3:0],addra[7:0],dina[31:0],douta[31:0]" */;
+  input clka;
+  input ena;
+  input [3:0]wea;
+  input [7:0]addra;
+  input [31:0]dina;
+  output [31:0]douta;
+endmodule
---
+++ func_test_v0.01/soc_axi_func/rtl/xilinx_ip/Data_RAM/Data_RAM_stub.vhdl
@@ -0,0 +1,35 @@
+-- Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2019.2 (win64) Build 2708876 Wed Nov  6 21:40:23 MST 2019
+-- Date        : Sat Jun  5 23:06:47 2021
+-- Host        : DESKTOP running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub
+--               e:/NSCSCC/func_test_v0.01/soc_axi_func/rtl/xilinx_ip/Data_RAM/Data_RAM_stub.vhdl
+-- Design      : Data_RAM
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7a200tfbg676-2
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity Data_RAM is
+  Port ( 
+    clka : in STD_LOGIC;
+    ena : in STD_LOGIC;
+    wea : in STD_LOGIC_VECTOR ( 3 downto 0 );
+    addra : in STD_LOGIC_VECTOR ( 7 downto 0 );
+    dina : in STD_LOGIC_VECTOR ( 31 downto 0 );
+    douta : out STD_LOGIC_VECTOR ( 31 downto 0 )
+  );
+
+end Data_RAM;
+
+architecture stub of Data_RAM is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clka,ena,wea[3:0],addra[7:0],dina[31:0],douta[31:0]";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "blk_mem_gen_v8_4_4,Vivado 2019.2";
+begin
+end;
