2293960281a2bbab5e519b92022d5a14ab36f47b
吴俊亮 <wujunliang18@mails.ucas.ac.cn>
2021-07-27 15:06:11 +0800

    fix

--- func_test_v0.01/soc_axi_func/rtl/myCPU/dcache.v
+++ func_test_v0.01/soc_axi_func/rtl/myCPU/dcache.v
@@ -726,7 +726,7 @@ assign addr_ok1 = (state == `IDLE || (state == `LOOKUP && cache_hit)) && _1_cach
 assign addr_ok2 = (state == `IDLE || (state == `LOOKUP && cache_hit)) && _2_cache_req &&
                   (!_1_cache_req || req_same_line && !req_read_write) ||
                   (state == `IDLE || (state == `LOOKUP && cache_hit)) && valid2 && uncache2 &&
-                  !(valid1 && uncache1);
+                  !valid1;
 assign data_ok1 = (state == `LOOKUP) && _1_cache_hit ||
                   (state == `REFILL) && rb_valid[0] && ret_valid || 
                   (state == `URRESP) && (uncache_way == 1'b0) && ret_valid ||
