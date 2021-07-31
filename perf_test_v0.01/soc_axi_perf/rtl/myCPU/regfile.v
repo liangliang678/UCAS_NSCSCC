d70a1c31d2e9dd51a729f6131758a8d05abd5386
吴俊亮 <wujunliang18@mails.ucas.ac.cn>
2021-07-31 14:38:03 +0800

    update regfile

--- perf_test_v0.01/soc_axi_perf/rtl/myCPU/regfile.v
+++ perf_test_v0.01/soc_axi_perf/rtl/myCPU/regfile.v
@@ -32,10 +32,170 @@ module regfile(
 reg [31:0] rf[31:0];
 
 //WRITE
+// always @(posedge clk) begin
+//     if (we_01) rf[waddr_01]<= wdata_01;
+//     if (we_02) rf[waddr_02]<= wdata_02;
+// end
 always @(posedge clk) begin
-    if (we_01) rf[waddr_01]<= wdata_01;
-    if (we_02) rf[waddr_02]<= wdata_02;
+    if ((waddr_02 == 5'b0) & we_02) rf[0]<= wdata_02;
+    else if((waddr_01 == 5'b0) & we_01) rf[0]<= wdata_01;
 end
+
+always @(posedge clk) begin
+    if ((waddr_02 == 5'b1) & we_02) rf[1]<= wdata_02;
+    else if((waddr_01 == 5'b1) & we_01) rf[1]<= wdata_01;
+end
+
+always @(posedge clk) begin
+    if ((waddr_02 == 5'b0) & we_02) rf[2]<= wdata_02;
+    else if((waddr_01 == 5'b0) & we_01) rf[2]<= wdata_01;
+end
+
+always @(posedge clk) begin
+    if ((waddr_02 == 5'b1) & we_02) rf[3]<= wdata_02;
+    else if((waddr_01 == 5'b1) & we_01) rf[3]<= wdata_01;
+end
+
+always @(posedge clk) begin
+    if ((waddr_02 == 5'b0) & we_02) rf[4]<= wdata_02;
+    else if((waddr_01 == 5'b0) & we_01) rf[4]<= wdata_01;
+end
+
+always @(posedge clk) begin
+    if ((waddr_02 == 5'b1) & we_02) rf[5]<= wdata_02;
+    else if((waddr_01 == 5'b1) & we_01) rf[5]<= wdata_01;
+end
+
+always @(posedge clk) begin
+    if ((waddr_02 == 5'b0) & we_02) rf[6]<= wdata_02;
+    else if((waddr_01 == 5'b0) & we_01) rf[6]<= wdata_01;
+end
+
+always @(posedge clk) begin
+    if ((waddr_02 == 5'b1) & we_02) rf[7]<= wdata_02;
+    else if((waddr_01 == 5'b1) & we_01) rf[7]<= wdata_01;
+end
+
+always @(posedge clk) begin
+    if ((waddr_02 == 5'b0) & we_02) rf[8]<= wdata_02;
+    else if((waddr_01 == 5'b0) & we_01) rf[8]<= wdata_01;
+end
+
+always @(posedge clk) begin
+    if ((waddr_02 == 5'b1) & we_02) rf[9]<= wdata_02;
+    else if((waddr_01 == 5'b1) & we_01) rf[9]<= wdata_01;
+end
+
+always @(posedge clk) begin
+    if ((waddr_02 == 5'b0) & we_02) rf[10]<= wdata_02;
+    else if((waddr_01 == 5'b0) & we_01) rf[10]<= wdata_01;
+end
+
+always @(posedge clk) begin
+    if ((waddr_02 == 5'b1) & we_02) rf[11]<= wdata_02;
+    else if((waddr_01 == 5'b1) & we_01) rf[11]<= wdata_01;
+end
+
+always @(posedge clk) begin
+    if ((waddr_02 == 5'b0) & we_02) rf[12]<= wdata_02;
+    else if((waddr_01 == 5'b0) & we_01) rf[12]<= wdata_01;
+end
+
+always @(posedge clk) begin
+    if ((waddr_02 == 5'b1) & we_02) rf[13]<= wdata_02;
+    else if((waddr_01 == 5'b1) & we_01) rf[13]<= wdata_01;
+end
+
+always @(posedge clk) begin
+    if ((waddr_02 == 5'b0) & we_02) rf[14]<= wdata_02;
+    else if((waddr_01 == 5'b0) & we_01) rf[14]<= wdata_01;
+end
+
+always @(posedge clk) begin
+    if ((waddr_02 == 5'b1) & we_02) rf[15]<= wdata_02;
+    else if((waddr_01 == 5'b1) & we_01) rf[15]<= wdata_01;
+end
+
+always @(posedge clk) begin
+    if ((waddr_02 == 5'b0) & we_02) rf[16]<= wdata_02;
+    else if((waddr_01 == 5'b0) & we_01) rf[16]<= wdata_01;
+end
+
+always @(posedge clk) begin
+    if ((waddr_02 == 5'b1) & we_02) rf[17]<= wdata_02;
+    else if((waddr_01 == 5'b1) & we_01) rf[17]<= wdata_01;
+end
+
+always @(posedge clk) begin
+    if ((waddr_02 == 5'b0) & we_02) rf[18]<= wdata_02;
+    else if((waddr_01 == 5'b0) & we_01) rf[18]<= wdata_01;
+end
+
+always @(posedge clk) begin
+    if ((waddr_02 == 5'b1) & we_02) rf[19]<= wdata_02;
+    else if((waddr_01 == 5'b1) & we_01) rf[19]<= wdata_01;
+end
+
+always @(posedge clk) begin
+    if ((waddr_02 == 5'b0) & we_02) rf[20]<= wdata_02;
+    else if((waddr_01 == 5'b0) & we_01) rf[20]<= wdata_01;
+end
+
+always @(posedge clk) begin
+    if ((waddr_02 == 5'b1) & we_02) rf[21]<= wdata_02;
+    else if((waddr_01 == 5'b1) & we_01) rf[21]<= wdata_01;
+end
+
+always @(posedge clk) begin
+    if ((waddr_02 == 5'b0) & we_02) rf[22]<= wdata_02;
+    else if((waddr_01 == 5'b0) & we_01) rf[22]<= wdata_01;
+end
+
+always @(posedge clk) begin
+    if ((waddr_02 == 5'b1) & we_02) rf[23]<= wdata_02;
+    else if((waddr_01 == 5'b1) & we_01) rf[23]<= wdata_01;
+end
+
+always @(posedge clk) begin
+    if ((waddr_02 == 5'b0) & we_02) rf[24]<= wdata_02;
+    else if((waddr_01 == 5'b0) & we_01) rf[24]<= wdata_01;
+end
+
+always @(posedge clk) begin
+    if ((waddr_02 == 5'b1) & we_02) rf[25]<= wdata_02;
+    else if((waddr_01 == 5'b1) & we_01) rf[25]<= wdata_01;
+end
+
+always @(posedge clk) begin
+    if ((waddr_02 == 5'b0) & we_02) rf[26]<= wdata_02;
+    else if((waddr_01 == 5'b0) & we_01) rf[26]<= wdata_01;
+end
+
+always @(posedge clk) begin
+    if ((waddr_02 == 5'b1) & we_02) rf[27]<= wdata_02;
+    else if((waddr_01 == 5'b1) & we_01) rf[27]<= wdata_01;
+end
+
+always @(posedge clk) begin
+    if ((waddr_02 == 5'b0) & we_02) rf[28]<= wdata_02;
+    else if((waddr_01 == 5'b0) & we_01) rf[28]<= wdata_01;
+end
+
+always @(posedge clk) begin
+    if ((waddr_02 == 5'b1) & we_02) rf[29]<= wdata_02;
+    else if((waddr_01 == 5'b1) & we_01) rf[29]<= wdata_01;
+end
+
+always @(posedge clk) begin
+    if ((waddr_02 == 5'b0) & we_02) rf[30]<= wdata_02;
+    else if((waddr_01 == 5'b0) & we_01) rf[30]<= wdata_01;
+end
+
+always @(posedge clk) begin
+    if ((waddr_02 == 5'b1) & we_02) rf[31]<= wdata_02;
+    else if((waddr_01 == 5'b1) & we_01) rf[31]<= wdata_01;
+end
+
 
 //READ OUT 1
 assign rdata_01 = (raddr_01 == 5'b0) ? 32'b0 : rf[raddr_01];
